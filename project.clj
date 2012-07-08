81abf528ac7082108ee6ec711aefced7e6a9e143
Takahiro Hozumi <fatrow+hozumi@googlemail.com>
2012-07-08 20:41:19 +0900

    :warn-on-reflection true

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "A detect encoding utility. juniversalchardet wrapper for Clojure."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.mozilla/juniversalchardet "1.0.3"]]
-  :repositories {"archive.org" "http://builds.archive.org:8080/maven2"})
+  :repositories {"archive.org" "http://builds.archive.org:8080/maven2"}
+  :warn-on-reflection true)
