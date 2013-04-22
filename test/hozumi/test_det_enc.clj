c898722b0027a6a3cb59dc09984f3af200204198
Dima Sabanin <sdmitry@gmail.com>
2013-04-22 11:01:27 -0400

    Added CP1251, CP1252, GBK, ISO2022JP to encoding tests

--- test/hozumi/test_det_enc.clj
+++ test/hozumi/test_det_enc.clj
@@ -6,9 +6,13 @@
 
 (deftest test-detect
   (are [expected target] (= expected (detect target))
-       "UTF-8"     "test/resources/utf_8.txt"
-       "SHIFT_JIS" "test/resources/shift_jis.txt"
-       "EUC-JP"    "test/resources/euc_jp.txt"
+       "UTF-8"        "test/resources/utf_8.txt"
+       "SHIFT_JIS"    "test/resources/shift_jis.txt"
+       "EUC-JP"       "test/resources/euc_jp.txt"
+       "WINDOWS-1251" "test/resources/cp1251.txt"
+       "ISO-2022-JP"  "test/resources/iso2022jp.txt"
+       "GB18030"      "test/resources/gbk.txt"
+       "WINDOWS-1252" "test/resources/latin1.txt"
        "UTF-8"     (File. "test/resources/utf_8.txt")
        "SHIFT_JIS" (File. "test/resources/shift_jis.txt")
        "EUC-JP"    (File. "test/resources/euc_jp.txt"))
