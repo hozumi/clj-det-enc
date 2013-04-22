173e1002e42f626334b3604ee2b54fbb35d506be
Dima Sabanin <sdmitry@gmail.com>
2013-04-22 11:12:45 -0400

    Reverting version back for easier merging upstream

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-det-enc "1.0.0"
+(defproject org.clojars.hozumi/clj-det-enc "1.0.0-SNAPSHOT"
   :description "A detect encoding utility. juniversalchardet wrapper for Clojure."
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.googlecode.juniversalchardet/juniversalchardet "1.0.3"]]
