1f446dc90b9cc77dbe08b704349f8f876022a0a0
Takahiro Hozumi <fatrow+hozumi@googlemail.com>
2013-04-23 20:19:41 +0900

    Refactoring

--- src/det_enc/core.clj
+++ src/det_enc/core.clj
@@ -1,15 +1,11 @@
-(ns
-    ^{:author "Takahiro Hozumi",
-      :doc "A detect encoding utility. juniversalchardet wrapper for Clojure."}
+(ns ^{:doc "A detect encoding utility. juniversalchardet wrapper for Clojure."}
   det-enc.core
-  (:require [clojure.java.io :as io :only [input-stream]])
-  (:import [org.mozilla.universalchardet UniversalDetector]
-           [java.io InputStream]
-           [java.nio.charset Charset]))
+  (:require [clojure.java.io :as io]))
 
-(defn- judge-seq! [buf ^InputStream istream ^UniversalDetector detector]
+(defn- judge-seq! [buf ^java.io.InputStream istream
+                   ^org.mozilla.universalchardet.UniversalDetector detector]
   (let [n        (.read istream buf)
-        proceed? (and (> n 0)
+        proceed? (and (< 0 n)
                       (not (do (.handleData detector buf 0 n)
                                (.isDone detector))))]
     (if proceed?
@@ -21,11 +17,11 @@
      (detect target nil))
   ([target encodingname-when-unknown]
      (let [buf      (make-array Byte/TYPE 4096)
-           detector (UniversalDetector. nil)]
+           detector (org.mozilla.universalchardet.UniversalDetector. nil)]
        (with-open [istream (io/input-stream target)]
          (dorun (judge-seq! buf istream detector))
          (.dataEnd detector)
          (or (.getDetectedCharset detector)
              (if (= encodingname-when-unknown :default)
-               (.displayName (Charset/defaultCharset))
+               (.displayName (java.nio.charset.Charset/defaultCharset))
                encodingname-when-unknown))))))
