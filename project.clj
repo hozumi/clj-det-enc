b95f0abc13ef54b5823378d346bfae1fbdec2444
Takahiro Hozumi <fatrow+hozumi@googlemail.com>
2013-04-23 19:38:52 +0900

    Revert clojure version to 1.2.0

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject org.clojars.hozumi/clj-det-enc "1.0.0-SNAPSHOT"
   :description "A detect encoding utility. juniversalchardet wrapper for Clojure."
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.2.0"]
                  [com.googlecode.juniversalchardet/juniversalchardet "1.0.3"]]
   :warn-on-reflection true)
