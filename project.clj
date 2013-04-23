2d7f2156dc7ca0be73547581f0714f54379510a9
Takahiro Hozumi <fatrow+hozumi@googlemail.com>
2013-04-23 20:22:24 +0900

    v1.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-det-enc "1.0.0-SNAPSHOT"
+(defproject clj-det-enc "1.0.0"
   :description "A detect encoding utility. juniversalchardet wrapper for Clojure."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [com.googlecode.juniversalchardet/juniversalchardet "1.0.3"]]
